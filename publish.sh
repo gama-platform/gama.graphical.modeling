3e884e4f57c4a24a0e933ceef93b7c024f2e1823
hqnghi88 <hqnghi88@gmail.com>
2019-09-12 11:47:02 +0700

    try to fix installation

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd idees.gama.p2site &&
-mvn clean install -P p2Repo --settings ../settings.xml && 
+cd idees.gama.p2site && 
+mvn -U clean install -P p2Repo --settings ../settings.xml && 
 cd -
